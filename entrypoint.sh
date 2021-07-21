eea92a8b2255eeebb56c75e41b5c8ab9254c5fe0
Peter Labick <Plabick@users.noreply.github.com>
2021-07-21 13:00:10 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,3 @@
-#!/bin/sh -c
+#!/bin/sh
 
-if [ ${#TEAM} -gt 0 ]
-then
-  python main.py $ACTION --token $TOKEN --names $NAMES --values $VALUES --team $TEAM
-else
-  python main.py $ACTION --token $TOKEN --names $NAMES --values $VALUES
-fi
+echo "Hello World"
