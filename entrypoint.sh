899c4f558a926dabcfe03f0d2899338bf0d66002
Peter Labick <Plabick@users.noreply.github.com>
2021-07-21 12:47:54 -0400

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+if [ ${#TEAM} -gt 0 ]
+then
+  python main.py $ACTION --token $TOKEN --names $NAMES --values $VALUES --team $TEAM
+else
+  python main.py $ACTION --token $TOKEN --names $NAMES --values $VALUES
+fi
