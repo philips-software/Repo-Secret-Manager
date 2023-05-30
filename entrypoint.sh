a0200164107e3d837b5d54b3ef2969b5156d92d0
kshitijzutshi222 <kshitij.zutshi@philips.com>
2023-05-30 11:51:40 -0400

    added new input in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,13 @@
 
 if [ ${#TEAM} -gt 0 ] && [ ${#REPO} -gt 0 ]
 then
-  python /app/main.py $ACTION --token $TOKEN --names $NAMES --values $VALUES --team $TEAM --repo $REPO
+  python /app/main.py $ACTION --token $TOKEN --names $NAMES --values $VALUES --team $TEAM --repo $REPO --dependabotsecretssync $DEPENDABOTSECRETSYNC
 elif [ ${#TEAM} -gt 0 ]
 then
-  python /app/main.py $ACTION --token $TOKEN --names $NAMES --values $VALUES --team $TEAM
+  python /app/main.py $ACTION --token $TOKEN --names $NAMES --values $VALUES --team $TEAM --dependabotsecretssync $DEPENDABOTSECRETSYNC
 elif [ ${#REPO} -gt 0 ]
 then
-  python /app/main.py $ACTION --token $TOKEN --names $NAMES --values $VALUES --repo $REPO
+  python /app/main.py $ACTION --token $TOKEN --names $NAMES --values $VALUES --repo $REPO --dependabotsecretssync $DEPENDABOTSECRETSYNC
 else
-  python /app/main.py $ACTION --token $TOKEN --names $NAMES --values $VALUES
+  python /app/main.py $ACTION --token $TOKEN --names $NAMES --values $VALUES --dependabotsecretssync $DEPENDABOTSECRETSYNC
 fi
