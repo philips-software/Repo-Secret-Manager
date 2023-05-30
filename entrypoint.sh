4b5f0159104106957bb768b3be559b49c3acffed
kshitijzutshi222 <kshitij.zutshi@philips.com>
2023-05-30 12:26:17 -0400

    if conditions chngs entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,21 @@
 if [ ${#TEAM} -gt 0 ] && [ ${#REPO} -gt 0 ] && [ ${#DEPENDABOTSECRETSYNC} -gt 0 ]
 then
   python /app/main.py $ACTION --token $TOKEN --names $NAMES --values $VALUES --team $TEAM --repo $REPO --dependabotsecretssync $DEPENDABOTSECRETSYNC
-elif [ ${#TEAM} -gt 0 ] && [ ${#DEPENDABOTSECRETSYNC} -gt 0 ]
+elif [ ${#TEAM} -gt 0 ] && [ ${#REPO} -gt 0 ]
 then
-  python /app/main.py $ACTION --token $TOKEN --names $NAMES --values $VALUES --team $TEAM --dependabotsecretssync $DEPENDABOTSECRETSYNC
+  python /app/main.py $ACTION --token $TOKEN --names $NAMES --values $VALUES --team $TEAM --repo $REPO
 elif [ ${#REPO} -gt 0 ] && [ ${#DEPENDABOTSECRETSYNC} -gt 0 ]
 then
   python /app/main.py $ACTION --token $TOKEN --names $NAMES --values $VALUES --repo $REPO --dependabotsecretssync $DEPENDABOTSECRETSYNC
+elif [ ${#TEAM} -gt 0 ] && [ ${#DEPENDABOTSECRETSYNC} -gt 0 ]
+then
+  python /app/main.py $ACTION --token $TOKEN --names $NAMES --values $VALUES --team $TEAM --dependabotsecretssync $DEPENDABOTSECRETSYNC
+elif [ ${#TEAM} -gt 0 ]
+then
+  python /app/main.py $ACTION --token $TOKEN --names $NAMES --values $VALUES --team $TEAM
+elif [ ${#REPO} -gt 0 ]
+then
+  python /app/main.py $ACTION --token $TOKEN --names $NAMES --values $VALUES --repo $REPO
 elif [ ${#DEPENDABOTSECRETSYNC} -gt 0 ]
 then
   python /app/main.py $ACTION --token $TOKEN --names $NAMES --values $VALUES --dependabotsecretssync $DEPENDABOTSECRETSYNC
