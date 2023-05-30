868eab8458778a7e0546b6adf7688416cddb3b23
kshitijzutshi222 <kshitij.zutshi@philips.com>
2023-05-30 12:05:36 -0400

    change to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
-if [ ${#TEAM} -gt 0 ] && [ ${#REPO} -gt 0 ]
+if [ ${#TEAM} -gt 0 ] && [ ${#REPO} -gt 0 ] && [ ${#DEPENDABOTSECRETSYNC} -gt 0 ]
 then
   python /app/main.py $ACTION --token $TOKEN --names $NAMES --values $VALUES --team $TEAM --repo $REPO --dependabotsecretssync $DEPENDABOTSECRETSYNC
-elif [ ${#TEAM} -gt 0 ]
+elif [ ${#TEAM} -gt 0 ] && [ ${#DEPENDABOTSECRETSYNC} -gt 0 ]
 then
   python /app/main.py $ACTION --token $TOKEN --names $NAMES --values $VALUES --team $TEAM --dependabotsecretssync $DEPENDABOTSECRETSYNC
-elif [ ${#REPO} -gt 0 ]
+elif [ ${#REPO} -gt 0 ] && [ ${#DEPENDABOTSECRETSYNC} -gt 0 ]
 then
   python /app/main.py $ACTION --token $TOKEN --names $NAMES --values $VALUES --repo $REPO --dependabotsecretssync $DEPENDABOTSECRETSYNC
-else
+elif [ ${#DEPENDABOTSECRETSYNC} -gt 0 ]
+then
   python /app/main.py $ACTION --token $TOKEN --names $NAMES --values $VALUES --dependabotsecretssync $DEPENDABOTSECRETSYNC
+else
+  python /app/main.py $ACTION --token $TOKEN --names $NAMES --values $VALUES
 fi
